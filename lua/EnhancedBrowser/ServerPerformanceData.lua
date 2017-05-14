f0da518694b39bfb05de667cd9902c66c5e7aaa0
stickz <stickman002@mail.com>
2017-05-14 15:35:50 -0400

    cleanup code

--- lua/EnhancedBrowser/ServerPerformanceData.lua
+++ lua/EnhancedBrowser/ServerPerformanceData.lua
@@ -11,7 +11,6 @@ local kPerfIconIcons = {
     PrecacheAsset("ui/icons/smiley_bad.dds")
 }
 
-<<<<<<< HEAD
 local kPingIconTextures = {
    PrecacheAsset("ui/icons/ping_5.dds"),
    PrecacheAsset("ui/icons/ping_4.dds"),
@@ -19,16 +18,6 @@ local kPingIconTextures = {
    PrecacheAsset("ui/icons/ping_2.dds"),
    PrecacheAsset("ui/icons/ping_1.dds")
 }
-=======
-function ServerPerformanceData.GetPerformanceIcon(quality, score)
-    local score = score * quality / kServerPerformanceDataUnknownQualityCutoff
-    local result = score <= kLoaded and kPerfIconIcons[3] or score <= kOk and kPerfIconIcons[2] or kPerfIconIcons[1]
-	
-    -- Fix for unknown performance ratings, so they're not rated red
-    if quality < kServerPerformanceDataUnknownQualityCutoff then
-	result = kPerfIconIcons[2]
-    end
->>>>>>> origin/master
 
 local kNormalPingPattern = { 60, 110, 180, 250,	999 }
 local kLoadedPingPattern = { 55, 100, 165, 225, 999 }
@@ -53,21 +42,11 @@ end
 -- return Unknown if quality is below cutoff (before that, the color
 -- of the text will still indicate score)
 function ServerPerformanceData.GetPerformanceText(quality, score)
-<<<<<<< HEAD
      if quality >= kServerPerformanceDataUnknownQualityCutoff then
         local score = score * quality / kServerPerformanceDataUnknownQualityCutoff
 		return 	 score <= kBad 	  and	Locale.ResolveString("SERVER_PERF_BAD") 	or
 				 score <= kLoaded and 	Locale.ResolveString("SERVER_PERF_LOADED")  or 
 										Locale.ResolveString("SERVER_PERF_GOOD")
-=======
-    local result = Locale.ResolveString("SERVER_PERF_UNKNOWN")
-    local score = score * quality / kServerPerformanceDataUnknownQualityCutoff
-    if quality >= kServerPerformanceDataUnknownQualityCutoff then
-        result = Locale.ResolveString(
-            ( score <= kLoaded and "SERVER_PERF_BAD" or
-            ( score <= kOk and "SERVER_PERF_LOADED" or
-            ( score <= kGood and "SERVER_PERF_OK" or "SERVER_PERF_GOOD"))))
->>>>>>> origin/master
     end
     
     return Locale.ResolveString("SERVER_PERF_UNKNOWN") -- return unknown if bellow quality cutoff
